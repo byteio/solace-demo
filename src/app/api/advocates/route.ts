ee6873749c64e50f74f6a8d4bd327cdebf271d4c
leo <leo@stanley.dev>
2025-10-22 22:14:03 -0500

    feat: update GET advocates endpoint to use full text search query

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,25 @@
+import { eq, sql } from "drizzle-orm";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import type { NextRequest } from "next/server";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
 
-  const data = advocateData;
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const rawQuery = searchParams.get("q");
+  const query = rawQuery?.trim();
+
+  if (!query) {
+    const data = await db.select().from(advocates).limit(50);
+    return Response.json({ data });
+  }
+  const data = await db
+    .select()
+    .from(advocates)
+    .where(
+      sql`${advocates.searchTsv} @@ websearch_to_tsquery('english', ${query})`,
+    )
+    .limit(50);
 
   return Response.json({ data });
 }
